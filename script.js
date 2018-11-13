f5c92de9bea3f9d3c00316375fa5bbee780dbad7
Krystian Pietron <krystianpietr@gmail.com>
2018-11-13 08:28:28 +0100

    add list and button cler for one task

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 class ToDo {
   constructor(container) {
     this.container = container;
-    this.tasks = []
+    this.tasks = [];
     this.renderElement();
+    this.render();
   }
 
-  render() {}
-  addTask() {}
-  renderElement() {
+  render() {
     this.container.innerHTML = "";
+    this.renderElement();
+    const ul = document.createElement("ul");
+    this.tasks.forEach((task, i) => {
+      const li = document.createElement("li");
+      const butt = document.createElement("button");
+      butt.innerText = "clear";
+      li.innerText = task;
+      ul.appendChild(li);
+      ul.appendChild(butt);
+      li.addEventListener("click", () => alert(`asd`));
+      butt.addEventListener("click", () => {
+        this.tasks.splice(i, 1);
+        this.render();
+      });
+    });
+    this.container.appendChild(ul)
+  }
+  addTask(text) {
+    this.text = text;
+    this.tasks.push(text);
+    this.render();
+  }
+
+  renderElement() {
     const inp = document.createElement("input");
     const but = document.createElement("button");
     but.innerText = "Dodaj";
-    but.addEventListener('click', ()=>{this.tasks.push(inp)})
+    but.addEventListener("click", () => {
+      this.addTask(inp.value);
+    });
     this.container.appendChild(inp);
     this.container.appendChild(but);
   }
